7fc15cf71f4cc79e31dd9b5cc9d5fb4381a302d2
Shen-Kit Hia <shenkit108+1@gmail.com>
2025-03-07 10:24:42 +1100

    ROFI updates rofi-pass script and enables fuzzy search

--- bashscripts/rofi-pass.sh
+++ bashscripts/rofi-pass.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
+##################################################
+#                 rofi-pass.sh                   #
+##################################################
+
+
 # get a list of services in the correct format
 services=$(find ~/.password-store/ -type f | awk -F '/' '($4 ~ "password") && ($5 !~ /^\..*/) { sub(/^.*store\//, ""); gsub(/\.gpg$/, ""); print }')
 
 # open rofi for selection, case-insensitive search
 selection=$(echo "$services" | rofi -dmenu -i)
+pwd=$(pass show "$selection" | head -1)
 
-# copy selection to clipboard
-pass -c "$selection"
+# if a selection was made, type the password of the selected service
+[[ "$pwd" != "Password Store" ]] && xdotool type "$pwd"
