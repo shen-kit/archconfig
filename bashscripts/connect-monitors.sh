d2685530482db6627ae686af8df5cdefcbe83e5f
Shen-Kit Hia <shenkit108+1@gmail.com>
2025-03-08 12:18:27 +1100

    BASHSCRIPTS updates connect-monitors.sh to use autorandr

--- bashscripts/connect-monitors.sh
+++ bashscripts/connect-monitors.sh
@@ -5,22 +5,11 @@
 # turn on connected monitors monitors and restart conky / awesomewm #
 #####################################################################
 
-pkill conky
-
-# start with all external displays off, turn on whatever wants to be turned on
-xrandr --output eDP-1 --mode 1280x720 --output DP-1 --off --output DP-2 --off --output DP-3 --off
-
-# left monitor
-if [[ ! -z $(xrandr | grep --invert-match "eDP-1" | grep "DP-1 connected") ]] then # connected
-   xrandr --output DP-1 --mode 1920x1080 --primary --left-of eDP-1
-fi
-
-# right monitor
-if [[ ! -z $(xrandr | grep --invert-match "eDP-1" | grep "DP-3 connected") ]] then # connected
-   xrandr --output DP-3 --mode 1920x1080 --primary --right-of eDP-1 
-fi
+# auto-detects connected hardware and sets xrandr settings based on saved profiles
+autorandr --change
 
 # restart conky and awesomewm
+pkill conky
 conky -c ~/.config/conky/conky.conf &
 awesome-client 'awesome.restart()'
 
--- bashscripts/disconnect-monitors.sh
+++ bashscripts/disconnect-monitors.sh
@@ -5,10 +5,9 @@
 # turn off monitors, turn on laptop, and restart conky / awesomewm #
 ####################################################################
 
-xrandr --output eDP-1 --mode 1280x720;
-xrandr --output DP-1 --off;
-xrandr --output DP-3 --off;
-xrandr --output DP-2 --off;
+xrandr --output eDP-1 --mode 1280x720 --output DP-1 --off --output DP-2 --off --output DP-3 --off;
+
+# restart conky and awesomewm
 pkill conky;
 conky & disown;
 awesome-client 'awesome.restart()'
