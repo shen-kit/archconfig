5e684dceb86f3674815a93274db5052415e97b54
Shen-Kit Hia <shenkit108+1@gmail.com>
2025-03-10 12:48:51 +1100

    NVIM adds formatters and updates markdown specific opts

--- dotfiles/nvim/after/ftplugin/markdown.lua
+++ dotfiles/nvim/after/ftplugin/markdown.lua
@@ -2,10 +2,14 @@
 
 vim.opt.breakindentopt = "list:2" -- wrap to same indentation as bullet lists
 
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
 -- ==========  KEYBINDS ==========
 
-local map              = vim.keymap.set
-local opts             = { noremap = true, silent = true }
+local map = vim.keymap.set
+local opts = { noremap = true, silent = true }
 
 -- bold text
 map("x", "<C-b>", "c****<ESC>hPgvllollo", opts)
@@ -18,10 +22,10 @@ map("x", "<C-3>", ":norm @c<CR>gv", opts) -- requires @c to be bound to :Obsidia
 
 -- obsidian
 map("n", "<C-S-O>", vim.cmd.ObsidianQuickSwitch, opts) -- quick switcher (Ctrl+Shift+O)
-map("n", "<leader>op", vim.cmd.ObsidianOpen, opts)     -- open in app
+map("n", "<leader>op", vim.cmd.ObsidianOpen, opts) -- open in app
 
 -- ==========  COMPLETION ==========
 -- remove completion engines except snippets
-require('cmp').setup.buffer({
-  sources = { { name = 'luasnip' } },
+require("cmp").setup.buffer({
+	sources = { { name = "luasnip" } },
 })
--- dotfiles/nvim/after/plugin/conform.lua
+++ dotfiles/nvim/after/plugin/conform.lua
@@ -1,13 +1,14 @@
 -- conform.lua (formatter)
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "black", "isort" },
-    javascript = { "prettierd", "prettier", stop_after_first = true },
-  },
-  format_on_save = {
-    timeout_ms = 100,
-    lsp_format = "fallback",
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "isort", "black" },
+		javascript = { "prettierd" },
+		c = { "clang-format" },
+		go = { "gofmt" },
+	},
+	format_on_save = {
+		timeout_ms = 250,
+		lsp_format = "fallback",
+	},
 })
-
