f2de0cbd4c58437ea15dd56ea55df98d01c03e0b
Shen-Kit Hia <shenkit108+1@gmail.com>
2025-04-03 21:10:14 +1100

    NVIM installs flash

--- dotfiles/nvim/after/ftplugin/markdown.lua
+++ dotfiles/nvim/after/ftplugin/markdown.lua
@@ -2,15 +2,17 @@
 
 local map = vim.keymap.set
 local opts = { silent = true, noremap = true }
+local opts_loud = { silent = true, noremap = true }
 
 -- bold / italic
 map("i", "<C-b>", "**", opts)
 map("v", "<C-b>", "s****<ESC>hP", opts) -- using mini.surround
 
--- obsidian
+-- ===== obsidian specific =====
+
 map({ "i", "n" }, "<C-S-O>", "<CMD>ObsidianQuickSwitch<CR>", opts)
--- paste image with default name
-map("n", "<leader>op", function()
+-- paste image with default name same as obsidian's naming
+map({ "i", "n" }, "<M-v>", function()
 	local fname = os.date("%Y%m%d%H%M%S") .. ".png"
 	vim.cmd("ObsidianPasteImg" .. fname)
-end, { silent = false, noremap = true })
+end, opts_loud)
---
+++ dotfiles/nvim/lua/plugins/flash.lua
@@ -0,0 +1,23 @@
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	opts = {},
+	keys = {
+		{
+			"m",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+		{
+			"r",
+			mode = "o",
+			function()
+				require("flash").remote()
+			end,
+			desc = "Remote Flash",
+		},
+	},
+}
