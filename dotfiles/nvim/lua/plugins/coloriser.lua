2f27caed969d68ead85bd43e5bf81d44ccd3c49e
Shen-Kit Hia <shenkit108@gmail.com>
2025-04-15 14:31:46 +1000

    NVIM adds autotag and coloriser plugins + fixes flash keybind overwriting mark

---
+++ dotfiles/nvim/lua/plugins/coloriser.lua
@@ -0,0 +1,2 @@
+-- highlights colours (e.g. hex codes) with background of the colour
+return { "norcalli/nvim-colorizer.lua" }
--- dotfiles/nvim/lua/plugins/flash.lua
+++ dotfiles/nvim/lua/plugins/flash.lua
@@ -4,7 +4,7 @@ return {
 	opts = {},
 	keys = {
 		{
-			"m",
+			"M",
 			mode = { "n", "x", "o" },
 			function()
 				require("flash").jump()
