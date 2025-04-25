fad08f9ab50bb161e9f932c8fa0fdd4f426f9903
Shen-Kit Hia <shenkit108@gmail.com>
2025-04-25 12:35:27 +1000

    NVIM adds jdtls-nvim

--- dotfiles/nvim/after/plugin/conform.lua
+++ dotfiles/nvim/after/plugin/conform.lua
@@ -1,13 +1,16 @@
 require("conform").setup({
-  formatters_by_ft = {
-    lua = { "stylua" },
-    python = { "isort", "black" },
-    javascript = { "prettierd" },
-    c = { "clang-format" },
-    go = { "gofmt" },
-  },
-  format_on_save = {
-    timeout_ms = 250,
-    lsp_format = "fallback",
-  },
+	formatters_by_ft = {
+		lua = { "stylua" },
+		python = { "isort", "black" },
+		javascript = { "prettierd" },
+		typescript = { "prettierd" },
+		javascriptreact = { "prettierd" },
+		typescriptreact = { "prettierd" },
+		c = { "clang-format" },
+		go = { "gofmt" },
+	},
+	format_on_save = {
+		timeout_ms = 250,
+		lsp_format = "fallback",
+	},
 })
