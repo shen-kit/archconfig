435ff0778de569909e046af9894b9279c152ef4f
Shen-Kit Hia <shenkit108@gmail.com>
2025-04-30 09:51:15 +1000

    NVIM fixes bug when opening java projects
    - was using both jdtls LSP and nvim-jdtls plugin
    - removes jdtls LSP as handled by nvim-jdtls

--- dotfiles/nvim/init.lua
+++ dotfiles/nvim/init.lua
@@ -9,5 +9,6 @@ require("config.lazy")
 require("config.pluginKeys")
 
 -- lsp / diagnostics
-vim.lsp.enable({ "clangd", "jdtls", "lua_ls", "ts_ls", "pyright", "gopls" })
+-- don't include jdtls as done with nvim-jdtls plugin
+vim.lsp.enable({ "clangd", "lua_ls", "ts_ls", "pyright", "gopls" })
 vim.diagnostic.config({ virtual_lines = { current_line = true } })
