c8ada3090c107a5f2c7689f9d4274c26a5ed9dbd
Shen-Kit Hia <shenkit108@gmail.com>
2025-05-19 23:38:43 +1000

    NVIM adds treesitter-context

--- dotfiles/nvim/lua/plugins/treesitter.lua
+++ dotfiles/nvim/lua/plugins/treesitter.lua
@@ -2,43 +2,68 @@ return {
 	{
 		"nvim-treesitter/nvim-treesitter",
 		build = ":TSUpdate",
-		config = function()
-			local configs = require("nvim-treesitter.configs")
-
-			configs.setup({
-				ensure_installed = {
-					"bash",
-					"c",
-					"css",
-					"html",
-					"java",
-					"javascript",
-					"json",
-					"lua",
-					"markdown",
-					"markdown_inline",
-					"python",
-					"query",
-					"regex",
-					"typescript",
-					"tsx",
-					"vim",
-					"vimdoc",
-					"yaml",
+		opts = {
+			ensure_installed = {
+				"bash",
+				"c",
+				"cpp",
+				"css",
+				"html",
+				"java",
+				"javascript",
+				"json",
+				"lua",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"yaml",
+			},
+			sync_install = false, -- install synchronously
+			auto_install = true, -- for missing parsers
+			highlight = {
+				enable = true,
+				-- disable treesitter for files over 256MB as it gets slow
+				disable = function(_, bufnr)
+					local buf_name = vim.api.nvim_buf_get_name(bufnr)
+					local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
+					return file_size > 256 * 1024
+				end,
+			},
+			indent = { enable = true },
+		},
+		dependencies = {
+			{
+				"nvim-treesitter/nvim-treesitter-context",
+				opts = {
+					max_lines = 3,
+					multiline_threshold = 1,
+					min_window_height = 20,
 				},
-				sync_install = false, -- install synchronously
-				auto_install = true, -- for missing parsers
-				highlight = {
-					enable = true,
-					-- disable treesitter for files over 256MB as it gets slow
-					disable = function(_, bufnr)
-						local buf_name = vim.api.nvim_buf_get_name(bufnr)
-						local file_size = vim.api.nvim_call_function("getfsize", { buf_name })
-						return file_size > 256 * 1024
-					end,
+				keys = {
+					{
+						"[c",
+						function()
+							-- Jump to previous change when in diffview.
+							if vim.wo.diff then
+								return "[c"
+							else
+								vim.schedule(function()
+									require("treesitter-context").go_to_context()
+								end)
+								return "<Ignore>"
+							end
+						end,
+						desc = "Jump to upper context",
+						expr = true,
+					},
 				},
-				indent = { enable = true },
-			})
-		end,
+			},
+		},
 	},
 }
