9f91ec86d656cead1714e5e2da659dd7fe1a6391
Shen-Kit Hia <shenkit108@gmail.com>
2026-05-31 15:10:00 +1000

    NVIM add flutter and pi.nvim plugins

---
+++ dotfiles/nvim/lua/plugins/ai-agents.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "pablopunk/pi.nvim",
+    config = function ()
+      require("pi").setup({
+        thinking = "off",
+      })
+
+      vim.keymap.set("n", "<leader>ai", ":PiAsk<CR>")
+      vim.keymap.set("v", "<leader>ai", ":PiAskSelection<CR>")
+    end
+  },
+}
